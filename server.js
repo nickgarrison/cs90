2055bbb8c5081936dd7dead4f4b1592da0813646
nickgarrison <nick.garrison1@gmail.com>
2016-10-31 13:56:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('Hello, world!');
+    res.end('Hello, world!');
     
 }).listen(process.env.PORT || 8080);
