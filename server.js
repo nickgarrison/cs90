964d5214e160b87f4a93052f2e3d430314da28e3
Garrison <0345995761@uscis.dhs.gov>
2017-02-28 15:45:54 -0500

    added info

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world! This is a test Azure deployment.');
+    res.end('Hello, world! This is a test Azure deployment. This was added on the development branch. ');
     
 }).listen(process.env.PORT || 8080);
